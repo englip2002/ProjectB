7f1bb3c0fdf481fd7f3c68310fe32bf1d530d7f3
Your Name <you@example.com>
2023-04-19 16:38:36 +0800

    singleton

--- src/FacadeDraw.java
+++ src/FacadeDraw.java
@@ -5,12 +5,23 @@ public class FacadeDraw {
     private Circle c;
     private Rectangular r;
     private Square s;
-    
+
+
+    //SINGLETON - To only create one object 
+    //SINGLETON STEP 2: object as attribute
+    private static FacadeDraw fds = new FacadeDraw();
+
     //constructor - to link facade to main components
-    public FacadeDraw(){
+    //SINGLETON STEP 1: private constuctor
+    private FacadeDraw(){
         c = new Circle();
         r = new Rectangular();
         s = new Square();
+    }
+
+    //SINGLETON STEP 3: Create entry point to access object
+    public static FacadeDraw getInstance(){
+        return fds;
     }
     
     public void drawCircle(){
--- src/ShapeClient.java
+++ src/ShapeClient.java
@@ -4,7 +4,15 @@ public class ShapeClient {
     public static void main(String[] args){
         //Circle objC = new Circle();
         //objC.draw();
-        FacadeDraw f = new FacadeDraw();
-        f.drawCircle();
+        //facade
+        //FacadeDraw f = new FacadeDraw()
+        //f.drawCircle();
+
+        //SINGLETON - To only create one object 
+        FacadeDraw fds = FacadeDraw.getInstance();
+        fds.drawCircle();
+        fds.drawRectangular();
+        fds.drawSquare();
+        
     }
 }
